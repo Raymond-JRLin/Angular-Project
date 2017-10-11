369e0b1bc09d0f965dd7c79e62ac4f57edc3247f
Junrui Lin <junrui@Junruis-MacBook-Pro.local>
2017-10-11 00:40:08 -0400

    add Food component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,39 @@
 import { Component } from '@angular/core';
 
+class Food {
+	name: string;
+	isFavorite: boolean;
+}
+
+const FOOD: Food[] = [
+	{
+		name: "Eggs",
+		isFavorite: true
+	},
+	{
+		name: "Milk",
+		isFavorite: false
+	},
+	{
+		name: "Jello",
+		isFavorite: true
+	}
+]
+
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  	selector: 'app-root',
+  	templateUrl: './app.component.html',
+  	styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  	title = 'hello word';
+  	// food: Food = {
+  	//    name: "Eggs",
+  	//    isFavorite: true,
+  	// }
+  	foods = FOOD;
+  	selectedFood: Food;
+  	onSelect(food: Food) : void {
+		this.selectedFood = food;
+  	}
 }
